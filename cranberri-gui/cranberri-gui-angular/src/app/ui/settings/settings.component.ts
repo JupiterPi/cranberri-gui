d5e089295e41e2ff624b936f753b8d6ea93877c5
JupiterPi <jupiterpi@outlook.de>
2023-09-04 17:51:03 +0200

    better size of settings window, styled scrollbars

--- cranberri-gui/cranberri-gui-angular/src/app/ui/settings/settings.component.ts
+++ cranberri-gui/cranberri-gui-angular/src/app/ui/settings/settings.component.ts
@@ -4,7 +4,7 @@ import {NeedsUpdateInfo, UpdateService} from "../../update.service";
 import {get, nonnull} from "../../util";
 
 export function openSettings() {
-  api.openWindow("settings", 500, 595, true);
+  api.openWindow("settings", 500, 400, true);
 }
 
 @Component({
